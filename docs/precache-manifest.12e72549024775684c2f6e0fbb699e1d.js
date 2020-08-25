65eeec018b1669012761ea80adb6b9e719336bef
Andrew Porter <mephisto83@hotmail.com>
2020-08-24 22:12:44 -0500

    updates docs

---
+++ docs/precache-manifest.12e72549024775684c2f6e0fbb699e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0a61774c273d07f51ab5376bf101e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f83411ce075c3ccfa7d",
+    "url": "/static/css/main.fe634123.chunk.css"
+  },
+  {
+    "revision": "7d676381561b81643291",
+    "url": "/static/js/2.0825a2a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0825a2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f83411ce075c3ccfa7d",
+    "url": "/static/js/main.f5f2c09d.chunk.js"
+  },
+  {
+    "revision": "60368744e9137a7ec19f",
+    "url": "/static/js/runtime-main.12ba74b3.js"
+  }
+]);
